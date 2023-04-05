efecb36fadbce9610e011209268de74d54204abe
Liliana Barrera <barrera.jimnlil@gmail.com>
2023-04-05 16:37:39 -0400

    Completed CC Form Adding Conditional UI error handling

--- Credit Card Form/src/Components/Form/Form.jsx
+++ Credit Card Form/src/Components/Form/Form.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import InputBase from "../InputBase/InputBase";
 import "./Form.css";
 import { OTHERCARDS } from "../constants";
-import { cardNumberValidation } from "../validations";
+import {
+  cardExpireValidation,
+  cardNumberValidation,
+  onlyTextValidation,
+  securityCodeValidation,
+} from "../validations";
 
 const INIT_CARD = {
   card: "",
@@ -48,19 +53,33 @@ class Form extends React.Component {
             cardError: errorText,
           },
         }));
-        // setState cardType and error
         break;
       case "cardHolder":
-        //checks for spaces and numbers
-        // setState error
+        errorText = onlyTextValidation(value);
+        this.setState((prevState) => ({
+          error: {
+            ...prevState.error,
+            cardHolderError: errorText,
+          },
+        }));
         break;
       case "expiry":
-        // check for date format
-        //setState error
+        errorText = cardExpireValidation(value);
+        this.setState((prevState) => ({
+          error: {
+            ...prevState.error,
+            expiryError: errorText,
+          },
+        }));
         break;
       case "securityCode":
-        // check for min length
-        //setState error
+        errorText = securityCodeValidation(3, value);
+        this.setState((prevState) => ({
+          error: {
+            ...prevState.error,
+            securityCodeError: errorText,
+          },
+        }));
         break;
       default:
         break;
@@ -100,10 +119,25 @@ class Form extends React.Component {
 
   render() {
     const inputData = [
-      { label: "Card Number", name: "card", type: "text" },
-      { label: "Cardholder's Name", name: "cardHolder", type: "text" },
-      { label: "Expiry Date (MM/YY)", name: "expiry", type: "text" },
-      { label: "Security Code", name: "securityCode", type: "text" },
+      { label: "Card Number", name: "card", type: "text", error: "cardError" },
+      {
+        label: "Cardholder's Name",
+        name: "cardHolder",
+        type: "text",
+        error: "cardHolderError",
+      },
+      {
+        label: "Expiry Date (MM/YY)",
+        name: "expiry",
+        type: "text",
+        error: "expiryError",
+      },
+      {
+        label: "Security Code",
+        name: "securityCode",
+        type: "text",
+        error: "securityCodeError",
+      },
     ];
 
     return (
@@ -121,6 +155,17 @@ class Form extends React.Component {
                   maxLength={this.state.maxLength}
                   name={item.name}
                   onBlur={this.handleBlur}
+                  error={this.state.error}
+                  cardType={this.state.cardType}
+                  isCard={item.name === "card"}
+                  errorM={
+                    this.state.error &&
+                    this.state.error[item.error] &&
+                    this.state.error[item.error].length > 1
+                      ? this.state.error[item.error]
+                      : null
+                  }
+                  // first we check for the object, then we check for the property, then if the length condition is true it will run
                 />
               ))
             : null}
